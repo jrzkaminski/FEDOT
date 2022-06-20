d4dfea29a3f3734332c47afcf8809b4b36a534e2
gkirgizov <gkirgizov@gmail.com>
2022-06-20 09:12:08 +0300

    WIP Extract Copyable base class, inheit optGraph from it

--- fedot/core/optimisers/graph.py
+++ fedot/core/optimisers/graph.py
@@ -5,12 +5,12 @@ from fedot.core.dag.graph_delegate import GraphDelegate
 from fedot.core.dag.graph_node import GraphNode
 from fedot.core.dag.graph_operator import GraphOperator
 from fedot.core.log import Log, default_log
-
+from fedot.core.utilities.data_structures import Copyable
 
 OptNode = GraphNode
 
 
-class OptGraph(GraphDelegate):
+class OptGraph(GraphDelegate, Copyable):
     """
     Base class used for optimized structure
 
@@ -22,17 +22,3 @@ class OptGraph(GraphDelegate):
                  log: Optional[Log] = None):
         self.log = log or default_log(__name__)
         super().__init__(GraphOperator(nodes))
-
-    def __copy__(self):
-        cls = self.__class__
-        result = cls.__new__(cls)
-        result.__dict__.update(self.__dict__)
-        return result
-
-    def __deepcopy__(self, memo=None):
-        cls = self.__class__
-        result = cls.__new__(cls)
-        memo[id(self)] = result
-        for k, v in self.__dict__.items():
-            setattr(result, k, deepcopy(v, memo))
-        return result
