709824ed329cd2980545c8b24dc9e65b4de14878
Grigorii Kirgizov <gkirgizov@gmail.com>
2022-09-15 19:32:55 +0300

    Fix without_tuning test for sever

--- fedot/api/api_utils/api_composer.py
+++ fedot/api/api_utils/api_composer.py
@@ -37,6 +37,8 @@ class ApiComposer:
         self.preprocessing_cache: Optional[PreprocessingCache] = None
         self.preset_name = None
         self.timer = None
+        # status flag indicating that tuner step was applied
+        self.was_tuned = False
 
     def obtain_metric(self, task: Task, metric: Union[str, Callable]) -> Sequence[MetricType]:
         """Chooses metric to use for quality assessment of pipeline during composition"""
@@ -212,11 +214,11 @@ class ApiComposer:
                                                                                      composer_requirements,
                                                                                      composer_params, log)
         if with_tuning:
-            self.tune_final_pipeline(task, train_data,
-                                     metric_functions[0],
-                                     composer_requirements,
-                                     best_pipeline,
-                                     log)
+            best_pipeline = self.tune_final_pipeline(task, train_data,
+                                                     metric_functions[0],
+                                                     composer_requirements,
+                                                     best_pipeline,
+                                                     log)
         # enforce memory cleaning
         gc.collect()
 
@@ -286,6 +288,7 @@ class ApiComposer:
             with self.timer.launch_tuning():
                 log.info('Hyperparameters tuning started')
                 tuned_pipeline = tuner.tune(pipeline_gp_composed)
+                self.was_tuned = True
                 log.info('Hyperparameters tuning finished')
         else:
             log.info(f'Time for pipeline composing was {str(self.timer.composing_spend_time)}.\n'
