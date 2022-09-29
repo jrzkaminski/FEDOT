485c6ad8f8226305c2d03dd1d3856465c7f0dac7
morrisnein <petroochcho@gmail.com>
2022-09-29 23:10:52 +0300

    allow `ParentOperator` get any sequence of `parent_individuals`

--- fedot/core/optimisers/opt_history/parent_operator.py
+++ fedot/core/optimisers/opt_history/parent_operator.py
@@ -1,24 +1,26 @@
 from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Tuple, Union
+from typing import Iterable, Tuple, Union
 from uuid import uuid4
 
 from fedot.core.optimisers.opt_history import Individual, IndividualsList
+from fedot.core.utilities.data_structures import ensure_wrapped_in_sequence
 
 
 @dataclass(frozen=True)
 class ParentOperator:
     type_: str
     operators: Union[Tuple[str, ...], str]
-    parent_individuals: Union[IndividualsList, Individual] = field()
+    parent_individuals: Union[IndividualsList, Individual, Iterable[Individual]] = field()
     uid: str = field(default_factory=lambda: str(uuid4()), init=False)
 
     def __post_init__(self):
         if isinstance(self.operators, str):
             object.__setattr__(self, 'operators', (self.operators,))
-        if isinstance(self.parent_individuals, Individual):
-            object.__setattr__(self, 'parent_individuals', IndividualsList([self.parent_individuals]))
+        if not isinstance(self.parent_individuals, IndividualsList):
+            parent_individuals = ensure_wrapped_in_sequence(self.parent_individuals)
+            object.__setattr__(self, 'parent_individuals', IndividualsList(parent_individuals))
 
     def __repr__(self):
         return (f'<ParentOperator {self.uid} | type: {self.type_} | operators: {self.operators} '
